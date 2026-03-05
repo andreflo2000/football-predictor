4e9d57a4418704aba7dd1e8ff9287fcc10bdfc96
andreflo2000 <florianparvu@yahoo.com>
2026-03-05 18:53:45 +0200

    Add environment variable for API URL

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,5 +3,8 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
+  env: {
+    NEXT_PUBLIC_API_URL: 'https://football-predictor-api-n9sl.onrender.com',
+  },
 }
 module.exports = nextConfig
